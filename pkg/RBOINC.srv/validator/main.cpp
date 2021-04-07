702d2b43487d472f56078d0e438a399d8d529ddb
prog_grey (Sergey Astaf'ev) <seryymail@mail.ru>
2021-04-07 09:20:34 +0000

    We abandoned RInside in the validator for the following reason: R does not return memory to the operating system even after object deletion and garbage collection. The new version of the validator uses the "system" C function and Rscript for validation.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rboinc@36 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/RBOINC.srv/validator/main.cpp
+++ pkg/RBOINC.srv/validator/main.cpp
@@ -1,41 +1,45 @@
 // Original file name: "main.cpp"
 // Created: 2021.03.30
-// Last modified: 2021.03.31
-// License: BSD-3-clause
+// Last modified: 2021.04.07
+// License: Comming soon
 // Written by: Astaf'ev Sergey <seryymail@mail.ru>
 // Description: This is universal validator for any R code results.
 // This is a part of RBOINC R package.
-// Copyright (c) 2021 Karelian Research Centre of the RAS:
-// Institute of Applied Mathematical Research
-// All rights reserved
-#include <RInside.h>
-#include <string>
-#include <iostream>
+#include <cstdlib>
+#include <cstring>
 #include <validate_util2.h>
 #include <validate_util.h>
 #include <error_numbers.h>
 #include <sched_util.h>
 #include <unistd.h>
+#include <limits.h>
 
-using namespace std;
 
 // R test script.
-string test_script = "\
+const char* test_script = "Rscript -e \"\
+file = commandArgs(trailingOnly=TRUE)[1]\n\
 result = tryCatch({\n\
   test_env = new.env()\n\
   load(file, test_env)\n\
-  if((length(test_env) == 1) && (!is.null(test_env$result))){\n\
+  if((length(test_env) == 1) && ('result' %in% names(test_env))){\n\
     TRUE\n\
   } else {\n\
     FALSE\n\
   }\n\
-}, error = function(cond){return(FALSE)}, warning = function(cond){return(FALSE)})";
+}, error = function(cond){return(FALSE)}, warning = function(cond){return(FALSE)})\n\
+if(result){\n\
+  quit('no', 0)\n\
+} else {\n\
+  quit('no', 1)\n\
+}\" \0";
 
-// R interpreter.
-RInside R_int;
+char *cmd_string;
 
 int validate_handler_init(int argc, char** argv)
 {
+	unsigned long int size = PATH_MAX + strlen(test_script);
+	cmd_string = (char*)malloc(size + 1);
+	memcpy((void*)cmd_string, (void*)test_script, strlen(test_script) + 1);
 	return 0;
 }
 
@@ -59,16 +63,9 @@ int init_result(RESULT& result, void*& data)
                 return ERR_FOPEN;
         }
         // Check file format.
-        R_int["file"] = filename;
-        R_int.parseEvalQ(test_script);
-	bool test_res = bool(R_int["result"]);
-	R_int.parseEvalQ("rm(list = ls())");
-        if(test_res == TRUE){
-                return 0;
-        } else {
-                return 1;
-        }
-	return 0;
+	memcpy((void*)(cmd_string + strlen(test_script)), (void*)filename, strlen(filename));
+	cmd_string[strlen(filename) + strlen(test_script)] = '\0';
+	return system(cmd_string);
 }
 
 int compare_results(RESULT& r1, void* data1, RESULT const& r2, void* data2, bool& match)
