bc2cbbc90e347b39fee149281e53308b7968a2cd
prog_grey (Sergey Astaf'ev) <seryymail@mail.ru>
2021-07-19 09:34:59 +0000

    Added full scripts for virtual machines.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rboinc@47 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ back-end/VM/boinc/boinc_app.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Original file name: "boinc_app.sh"
+# Created: 2021.07.13
+# Last modified: 2021.07.13
+# License: BSD-3-clause
+# Written by: Astaf'ev Sergey <seryymail@mail.ru>
+# This is a part of RBOINC R package.
+# Copyright (c) 2021 Karelian Research Centre of the RAS:
+# Institute of Applied Mathematical Research
+# All rights reserved
+
+echo "Press Ctrl-Alt-(F2-F6) to switch to another terminal. root password is \"0000\""
---
+++ back-end/VM/root/prepare_release.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Original file name: "prepare_release.sh"
+# Created: 2021.06.03
+# Last modified: 2021.07.14
+# License: BSD-3-clause
+# Written by: Astaf'ev Sergey <seryymail@mail.ru>
+# This is a part of RBOINC R package.
+# Copyright (c) 2021 Karelian Research Centre of the RAS:
+# Institute of Applied Mathematical Research
+# All rights reserved
+
+echo "Deleting packages:"
+emerge --depclean --deep
+echo "Cleaning portage cache:"
+eclean distfiles
+eclean packages
+rm /var/cache/distfiles/*
+echo "Cleaning logs:"
+truncate -s 0 /var/log/emerge.log
+truncate -s 0 /var/log/emerge-fetch.log
+rm -r /var/log/portage/*
+echo "Defragment file system:"
+btrfs filesystem defragment -r /
+echo "Rebalance btrfs:"
+btrfs balance start --full-balance /
+echo -e "\033[32mCleaning completed. Use Clonezilla to reduce size.\033[0m"
